f12e27aa716f235c54e5b4e0a110c4089809d7f7
ShrewdSpirit <mohammad.zardoshtian@hotmail.com>
2020-11-04 04:47:43 +0330

    improved server functionality a bit

--- main.go
+++ main.go
@@ -1,48 +1,51 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-    var port int
-    var wwwroot string
+	var port int
+	var wwwroot string
+	var servedir bool
 
-    wd, err := os.Getwd()
-    exitError(err)
+	wd, err := os.Getwd()
+	exitError(err)
 
-    flag.IntVar(&port, "port", 6969, "listen port")
-    flag.StringVar(&wwwroot, "root", wd, "serve root directory (defaults to current directory)")
-    flag.Parse()
+	flag.BoolVar(&servedir, "servedir", false, "Enable serving directories")
+	flag.IntVar(&port, "port", 6969, "listen port")
+	flag.StringVar(&wwwroot, "root", wd, "serve root directory (defaults to current directory)")
+	flag.Parse()
 
-    go func() {
-        fmt.Printf("serving directory %s\n", wwwroot)
-        fmt.Printf("listening on :%d\n", port)
+	go func() {
+		fmt.Printf("serving directory %s\n", wwwroot)
+		fmt.Printf("listening on :%d\n", port)
 
-        s := &SimpleServe{
-            WWWRoot: wwwroot,
-        }
+		s := &SimpleServe{
+			WWWRoot:  wwwroot,
+			ServeDir: servedir,
+		}
 
-        if err := http.ListenAndServe(fmt.Sprintf(":%d", port), s); err != nil {
-            exitError(err)
-        }
-    }()
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", port), s); err != nil {
+			exitError(err)
+		}
+	}()
 
-    exit := make(chan os.Signal, 1)
-    signal.Notify(exit, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGINT)
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGINT)
 
-    <-exit
-    fmt.Println("exit")
+	<-exit
+	fmt.Println("exit")
 }
 
 func exitError(err error) {
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- server.go
+++ server.go
@@ -1,68 +1,119 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "mime"
-    "net/http"
-    "os"
-    "path/filepath"
-    "strconv"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 type LogType string
 
 const (
-    LogTypeInfo LogType = "Info"
-    LogTypeErr  LogType = "Error"
+	LogTypeInfo LogType = "Info"
+	LogTypeErr  LogType = "Error"
 )
 
 type SimpleServe struct {
-    WWWRoot string
+	WWWRoot  string
+	ServeDir bool
 }
 
 func (s *SimpleServe) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-    path := strings.Trim(req.URL.Path, "/")
-    if path == "" {
-        path = "index.html"
-    }
+	path := filepath.Join(s.WWWRoot, strings.Trim(req.URL.Path, "/"))
 
-    path = filepath.Join(s.WWWRoot, path)
+	stat, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := s.serveIndex(w, path); err != nil {
+				s.log(nil, req, LogTypeErr, err.Error())
+			}
 
-    stat, err := os.Stat(path)
-    if err != nil {
-        w.WriteHeader(http.StatusNotFound)
-        s.log(nil, req, LogTypeErr, err.Error())
-        return
-    }
+			return
+		}
 
-    if stat.IsDir() {
-        w.WriteHeader(http.StatusNotImplemented)
-    } else {
-        file, err := os.Open(path)
-        if err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
-            s.log(w, req, LogTypeErr, err.Error())
-            return
-        }
-        defer file.Close()
+		w.WriteHeader(http.StatusInternalServerError)
+		s.log(nil, req, LogTypeErr, err.Error())
+		return
+	}
 
-        w.Header().Set("Content-Type", mime.TypeByExtension(path))
-        w.Header().Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
+	if stat.IsDir() {
+		if !s.ServeDir {
+			w.WriteHeader(http.StatusUnauthorized)
+		} else {
+			if err := s.serveDir(w, path); err != nil {
+				s.log(nil, req, LogTypeErr, err.Error())
+			}
+		}
 
-        if _, err := io.Copy(w, file); err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
-            s.log(w, req, LogTypeErr, err.Error())
-            return
-        }
-    }
+		return
+	}
+
+	if err := s.serveFile(w, path, stat.Size()); err != nil {
+		s.log(nil, req, LogTypeErr, err.Error())
+		return
+	}
+}
+
+func (s *SimpleServe) serveIndex(w http.ResponseWriter, path string) error {
+	path = filepath.Join(path, "index.html")
+	stat, err := os.Stat(path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	return s.serveFile(w, path, stat.Size())
+}
+
+func (s *SimpleServe) serveDir(w http.ResponseWriter, path string) error {
+	ls, err := ioutil.ReadDir(path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	responseBytes, err := json.Marshal(ls)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	w.Write(responseBytes)
+
+	return nil
+}
+
+func (s *SimpleServe) serveFile(w http.ResponseWriter, path string, length int64) error {
+	file, err := os.Open(path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", mime.TypeByExtension(path))
+	w.Header().Set("Content-Length", strconv.FormatInt(length, 10))
+
+	if _, err := io.Copy(w, file); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
 }
 
 func (s *SimpleServe) log(to http.ResponseWriter, req *http.Request, logtype LogType, msg string) {
-    text := fmt.Sprintf("[%s] %s\n\t%s", logtype, msg, req.URL.String())
-    fmt.Println(text)
-    if to != nil {
-        to.Write([]byte(text))
-    }
+	text := fmt.Sprintf("[%s] [at %s] %s", logtype, req.URL.String(), msg)
+	log.Println(text)
+	if to != nil {
+		to.Write([]byte(text))
+	}
 }
